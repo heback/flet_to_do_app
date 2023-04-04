7371947a85137670f7c067da9e961d656407dad0
heback <heback@gmail.com>
2023-04-04 16:21:47 +0900

    2023-04-04 16:21

--- to_do_sqlite.py
+++ to_do_sqlite.py
@@ -1,14 +1,59 @@
 from flet import *
+import sqlite3
+
+
+class Database:
+
+    @staticmethod
+    def connectToDatabase():
+        try:
+            db = sqlite3.connect('todo.db')
+            c = db.cursor()
+            c.execute('CREATE TABLE IF NOT EXISTS tasks '
+                      '(id INTEGER PRIMARY KEY AUTOINCREMENT,'
+                      'task TEXT NOT NULL,'
+                      'reg_date TEXT NOT NULL)')
+            return db
+        except Exception as e:
+            print(e)
+
+    def readDatabase(db):
+        c = db.cursor()
+        c.execute('SELECT id, task, date FROM tasks')
+        res = c.fetchall()
+        return res
+
+    def insertDatabase(db, values):
+        c = db.cursor()
+        c.execute('INSERT INTO tasks (task, date) VALUES (?, ?)', values)
+        db.commit()
+        return c.lastrowid
+
+    def deleteDatabase(db, value):
+        c = db.cursor()
+        c.execute('DELETE FROM tasks WHERE id=?', value)
+        db.commit()
+
+    def updateDatabase(db, values):
+        c = db.cursor()
+        c.execute('UPDATE tasks SET task=? WHERE id=?', values)
+        db.commit()
 
 
 class Task(UserControl):
-    def __init__(self, task_name, task_delete):
+
+    def __init__(self, task_name, task_status_change, task_delete):
         super().__init__()
+        self.completed = False
         self.task_name = task_name
+        self.task_status_change = task_status_change
         self.task_delete = task_delete
 
     def build(self):
-        self.display_task = Checkbox(value=False, label=self.task_name)
+
+        self.display_task = Checkbox(
+            value=False, label=self.task_name, on_change=self.status_changed
+        )
         self.edit_name = TextField(expand=1)
 
         self.display_view = Row(
@@ -62,6 +107,10 @@ class Task(UserControl):
         self.edit_view.visible = False
         self.update()
 
+    def status_changed(self, e):
+        self.completed = self.display_task.value
+        self.task_status_change(self)
+
     def delete_clicked(self, e):
         self.task_delete(self)
 
@@ -70,6 +119,12 @@ class TodoApp(UserControl):
     def build(self):
         self.new_task = TextField(hint_text="Whats needs to be done?", expand=True)
         self.tasks = Column()
+
+        self.filter = Tabs(
+            selected_index=0,
+            on_change=self.tabs_changed,
+            tabs=[Tab(text="all"), Tab(text="active"), Tab(text="completed")],
+        )
 
         # application's root control (i.e. "view") containing all other controls
         return Column(
@@ -81,24 +136,50 @@ class TodoApp(UserControl):
                         FloatingActionButton(icon=icons.ADD, on_click=self.add_clicked),
                     ],
                 ),
-                self.tasks,
+                Column(
+                    spacing=25,
+                    controls=[
+                        self.filter,
+                        self.tasks,
+                    ],
+                ),
             ],
         )
 
     def add_clicked(self, e):
-        task = Task(self.new_task.value, self.task_delete)
-        self.tasks.controls.append(task)
+        task = Task(self.new_task.value, self.task_status_change, self.task_delete)
+        self.tasks.controls.insert(0, task)
         self.new_task.value = ""
+        self.update()
+
+    def task_status_change(self, task):
         self.update()
 
     def task_delete(self, task):
         self.tasks.controls.remove(task)
         self.update()
 
+    def update(self):
+        status = self.filter.tabs[self.filter.selected_index].text
+        for task in self.tasks.controls:
+            task.visible = (
+                status == "all"
+                or (status == "active" and task.completed == False)
+                or (status == "completed" and task.completed)
+            )
+        super().update()
+
+    def tabs_changed(self, e):
+        self.update()
+
 
 def main(page: Page):
     page.title = "ToDo App"
     page.horizontal_alignment = "center"
+    page.window_width = 500
+    page.window_height = 600
+    page.window_maximizable = False
+    page.window_minimizable = False
     page.update()
 
     # create application instance
