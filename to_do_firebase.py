c28fad452a6b4c92750e367c0031bde7e183c43d
heback <heback@gmail.com>
2023-04-11 15:15:36 +0900

    2023-04-11 15:15

---
+++ to_do_firebase.py
@@ -0,0 +1,190 @@
+from flet import *
+import datetime
+
+# pip install firebase-admin
+# firebase console에 로그인
+# 프로젝트 생성
+# 프로젝트 설정 > 서비스 계정 메뉴로 가서
+# 파이선 코드 복사해서 가져오고 private key 생성 후 프로젝트 폴더로 복사
+
+class Task(UserControl):
+
+    def __init__(self, task_name, task_completed, task_date, task_status_change, task_delete, task_id = None):
+        self.task_name = task_name
+        self.task_completed = task_completed
+        self.task_date = task_date
+        self.task_status_change = task_status_change
+        self.task_delete = task_delete
+        self.task_id = task_id
+        super().__init__()
+
+    def build(self):
+
+        self.display_task = Checkbox(
+            value=self.task_completed,
+            label=self.task_name,
+            on_change=self.status_changed,
+        )
+        self.edit_name = TextField(expand=1)
+
+        self.display_view = Row(
+            alignment="spaceBetween",
+            vertical_alignment="center",
+            controls=[
+                Column(
+                    controls=[
+                        self.display_task,
+                        Text(
+                            str(self.task_date)[:16],
+                        )
+                    ]),
+                Row(
+                    spacing=0,
+                    controls=[
+                        IconButton(
+                            icon=icons.CREATE_OUTLINED,
+                            tooltip="Edit To-Do",
+                            on_click=self.edit_clicked,
+                        ),
+                        IconButton(
+                            icons.DELETE_OUTLINE,
+                            tooltip="Delete To-Do",
+                            on_click=self.delete_clicked,
+                        ),
+                    ],
+                ),
+            ],
+        )
+
+        self.edit_view = Row(
+            visible=False,
+            alignment="spaceBetween",
+            vertical_alignment="center",
+            controls=[
+                self.edit_name,
+                IconButton(
+                    icon=icons.DONE_OUTLINE_OUTLINED,
+                    icon_color=colors.GREEN,
+                    tooltip="Update To-Do",
+                    on_click=self.save_clicked,
+                ),
+            ],
+        )
+        return Column(controls=[self.display_view, self.edit_view])
+
+    def edit_clicked(self, e):
+        self.edit_name.value = self.display_task.label
+        self.display_view.visible = False
+        self.edit_view.visible = True
+        self.update()
+
+    def save_clicked(self, e):
+        self.display_task.label = self.edit_name.value
+        self.display_view.visible = True
+        self.edit_view.visible = False
+
+        self.task_name = self.edit_name.value
+        db.updateDatabase((self.task_name, self.task_id))
+        self.update()
+
+    def status_changed(self, e):
+        self.task_completed = self.display_task.value
+        self.task_status_change(self)
+        self.update()
+
+    def delete_clicked(self, e):
+        db.deleteDatabase(self.task_id)
+        self.task_delete(self)
+        self.update()
+
+
+class TodoApp(UserControl):
+
+    def build(self):
+
+        task_list = db.readDatabase()
+
+        self.new_task = TextField(hint_text="Whats needs to be done?", expand=True)
+        self.tasks = Column()
+
+        for t in task_list:
+            task = Task(t[1], bool(t[2]), t[3], self.task_status_change, self.task_delete, t[0])
+            self.tasks.controls.insert(0, task)
+
+        self.filter = Tabs(
+            selected_index=0,
+            on_change=self.tabs_changed,
+            tabs=[Tab(text="all"), Tab(text="active"), Tab(text="completed")],
+        )
+
+        # application's root control (i.e. "view") containing all other controls
+        return Column(
+            width=600,
+            height=500,
+            controls=[
+                Row(
+                    controls=[
+                        self.new_task,
+                        FloatingActionButton(icon=icons.ADD, on_click=self.add_clicked),
+                    ],
+                ),
+                Column(
+                    spacing=25,
+                    controls=[
+                        self.filter,
+                        self.tasks,
+                    ],
+                ),
+            ],
+
+            scroll=True
+        )
+
+    def add_clicked(self, e):
+        t = datetime.datetime.now()
+        id = db.insertDatabase([self.new_task.value, t])
+        task = Task(self.new_task.value, False, t, self.task_status_change, self.task_delete, id)
+        self.tasks.controls.insert(0, task)
+        self.new_task.value = ""
+        self.update()
+
+    def task_status_change(self, task):
+        # task 상태 업데이트
+        db.updateTaskState(task.task_id, task.task_completed)
+        self.update()
+
+    def task_delete(self, task):
+        self.tasks.controls.remove(task)
+        self.update()
+
+    def update(self):
+        status = self.filter.tabs[self.filter.selected_index].text
+        for task in self.tasks.controls:
+            task.visible = (
+                status == "all"
+                or (status == "active" and task.task_completed == False)
+                or (status == "completed" and task.task_completed)
+            )
+        super().update()
+
+    def tabs_changed(self, e):
+        self.update()
+
+
+def main(page: Page):
+    page.title = "ToDo App"
+    page.horizontal_alignment = "center"
+    page.window_width = 500
+    page.window_height = 600
+    page.window_maximizable = False
+    page.window_minimizable = False
+    page.update()
+
+    # create application instance
+    app = TodoApp(db)
+
+    # add application's root control to the page
+    page.add(app)
+
+
+flet.app(target=main)
